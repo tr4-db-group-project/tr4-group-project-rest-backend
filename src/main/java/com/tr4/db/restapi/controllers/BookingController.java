5fdb6a39ac4122ad1ff480523191ab6dfba0364b
Gloal <alori.glo@gmail.com>
2024-06-24 21:10:58 +0100

    basic rest api reading input

--- src/main/java/com/tr4/db/restapi/controllers/BookingController.java
+++ src/main/java/com/tr4/db/restapi/controllers/BookingController.java
@@ -1,12 +1,17 @@
 package com.tr4.db.restapi.controllers;
 
 import com.tr4.db.restapi.model.Booking;
+import org.apache.coyote.http11.upgrade.UpgradeServletOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/booking")
 public class BookingController {
+
+    private final Logger logger = LoggerFactory.getLogger(BookingController.class);
 
     @GetMapping("")
     public ResponseEntity<?> getBooking(){
@@ -15,6 +20,7 @@ public class BookingController {
 
     @PostMapping("")
     public ResponseEntity<Booking> postRequest(@RequestBody Booking booking) {
+        logger.info("Booking received: {}",booking);
         return ResponseEntity.ok(booking);
     }
 }
--- src/main/java/com/tr4/db/restapi/service/BookingService.java
+++ src/main/java/com/tr4/db/restapi/service/BookingService.java
@@ -1,4 +1,9 @@
 package com.tr4.db.restapi.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookingService {
+
+
 }
