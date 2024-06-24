2a04bc5cea6238ab713d07fec66923c7320db531
Smitha <smitha.kotian@gmail.com>
2024-06-24 13:35:04 +0100

    feat:Initial Project setup

---
+++ src/main/java/com/tr4/db/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package com.tr4.db.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
